f18614abe9eb49d954bbaab4b6db7fd1e8451bb3
mualjafari <muath.aljafari@mbc.net>
2018-02-06 12:01:06 +0200

    add dummy controller

--- src/main/java/com/ebooking/controllers/OffersController.java
+++ src/main/java/com/ebooking/controllers/OffersController.java
@@ -12,7 +12,10 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @Controller
 public class OffersController {
@@ -45,7 +48,8 @@ public class OffersController {
     public @ResponseBody RootObject event(@RequestBody RootObject rootObject, HttpServletRequest httpRequest) throws IOException {
         System.out.println("request>>>>>>>"+httpRequest.toString());
 
-        httpRequest.getReader().lines().forEach(System.out::println);
+        String body = getBody(httpRequest);
+        System.out.println(body);
         System.out.println(rootObject.getMessage().getData());
         System.out.println(rootObject);
         return rootObject;
@@ -54,6 +58,40 @@ public class OffersController {
     @RequestMapping(value = "/show/googleaaf6a1bff784af77.html", method = RequestMethod.GET)
     public @ResponseBody String show() {
         return "google-site-verification: googleaaf6a1bff784af77.html";
+    }
+
+    public static String getBody(HttpServletRequest request) throws IOException {
+
+        String body = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = null;
+
+        try {
+            InputStream inputStream = request.getInputStream();
+            if (inputStream != null) {
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                char[] charBuffer = new char[128];
+                int bytesRead = -1;
+                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+                    stringBuilder.append(charBuffer, 0, bytesRead);
+                }
+            } else {
+                stringBuilder.append("");
+            }
+        } catch (IOException ex) {
+            throw ex;
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException ex) {
+                    throw ex;
+                }
+            }
+        }
+
+        body = stringBuilder.toString();
+        return body;
     }
 
 
